2617bec6670376e0d99b289d9db91596e9e6d0d5
L.J. Hill <foehawk@gmail.com>
2016-01-26 18:51:36 -0500

    Fix config tests for Python 3

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,13 @@
 from coffer import config
-from ConfigParser import SafeConfigParser
 import mock
 import os
 import tempfile
+import sys
+if sys.version_info[0] < 3:
+    from ConfigParser import SafeConfigParser
+else:
+    from configparser import SafeConfigParser
+    basestring = str
 
 def expand_string(s, *args, **kwargs):
     return os.path.expanduser(os.path.expandvars(s))
@@ -11,18 +16,18 @@ def assert_config_equals(cfg, d, *args, **kwargs):
     cfg_dict = {}
     for section in cfg.sections():
         for option in cfg.options(section):
-            if not cfg_dict.has_key(section):
+            if not section in cfg_dict:
                 cfg_dict[section] = {}
             cfg_dict[section][option] = cfg.get(section, option)
 
-    for k, v in d.iteritems():
+    for k, v in d.items():
         k = k.lower()
-        assert cfg_dict.has_key(k)
+        assert k in cfg_dict
         if isinstance(v, dict):
-            for kk, vv in v.iteritems():
+            for kk, vv in v.items():
                 kk = kk.lower()
                 assert isinstance(cfg_dict[k], dict)
-                assert cfg_dict[k].has_key(kk)
+                assert kk in cfg_dict[k]
                 assert expand_string(cfg_dict[k][kk]) == expand_string(vv)
         else:
             assert expand_string(d[k]) == expand_string(v)
@@ -48,7 +53,7 @@ def test_load_default_config(*args, **kwargs):
 
 def test_load_custom_config(*args, **kwargs):
     with tempfile.NamedTemporaryFile() as tmpfile:
-        tmpfile.write('[interactive]\nprompt = test>\n')
+        tmpfile.write(b'[interactive]\nprompt = test>\n')
         custom_cfg_dict = {'interactive': {'prompt': 'test>'}}
         custom_cfg_dict.update(config._defaults)
         environ = {'COFFER_CONFIG': tmpfile.name}
